eb92072497de133316e48e5e8ccdb2dab804223c
Lillymuller <eleltaalemu@gmail.com>
2023-12-13 09:18:36 -0800

    free_fun

---
+++ free_fun.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+*free_fun - free all linked list
+*@stack: pointer to the first node
+*/
+
+void free_fun(stack_t *stack)
+{
+	stack_t *top;
+
+	while (stack != NULL)
+	{
+		top = stack->next;
+		free(stack);
+		stack = top;
+	}
+}
