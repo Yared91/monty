e578b3a71b5556df4a203f8507fc0bd6e6a8fc7c
Lillymuller <eleltaalemu@gmail.com>
2023-12-15 00:26:32 -0800

    Task_11

--- Task_11.c
+++ Task_11.c
@@ -9,9 +9,9 @@
 
 void m_pchar(stack_t **stack, unsigned int line_number)
 {
-if (!stack)
+if (!stack || !*stack)
 {
-fprintf(stderr, "L%d: can't pchar, value out of range\n",
+fprintf(stderr, "L%d: can't pchar, stack empty\n",
 line_number);
 exit(EXIT_FAILURE);
 }
@@ -22,7 +22,7 @@ printf("%c\n", (*stack)->n)
 }
 else
 {
-fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 exit(EXIT_FAILURE);
 }
 }
